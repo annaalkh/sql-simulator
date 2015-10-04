3f41b88c455211e5765e82d8330d490f51637e66
annaalkh <annaalkh@yandex.ru>
2015-10-04 20:59:38 +0400

    initial project structure

---
+++ src/main/java/ru/hse/sqlsimulator/web/TestPageController.java
@@ -0,0 +1,16 @@
+package ru.hse.sqlsimulator.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Anna on 10/4/2015.
+ */
+@Controller
+public class TestPageController {
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
